21a4578db10d2c6cb4426824c8a9f4605d3a7d1d
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-06 14:00:08 +0200

    fix conflict with model:user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-<<<<<<< HEAD
   devise :database_authenticatable, :jwt_authenticatable, :registerable,
          :recoverable, jwt_revocation_strategy: JwtDenylist
-
-
-=======
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
   has_many :real_estate_ads, dependent: :destroy      
->>>>>>> develop
 end
