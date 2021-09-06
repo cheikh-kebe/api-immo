dde84afda3fd59145b7f8eacc09ff2f0fa59bc07
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-06 11:49:49 +0200

    fix ads init

--- db/migrate/20210906092746_create_ereal_estate_adds.rb
+++ db/migrate/20210906092746_create_ereal_estate_adds.rb
@@ -1,9 +1,9 @@
 class CreateErealEstateAdds < ActiveRecord::Migration[6.1]
   def change
-    create_table :ereal_estate_adds do |t|
+    create_table :real_estate_adds do |t|
       t.string :title
       t.text :description
-      t.int :price
+      t.integer :price
 
       t.timestamps
     end
