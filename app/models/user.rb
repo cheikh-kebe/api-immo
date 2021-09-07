711e73423067962f3f16de3dc7327d6ed09ade77
shanks <cheikh.kebe.01@gmail.com>
2021-09-07 11:07:46 +0200

    action mailer for the signup configured works with letter opener ans setted sendgrid for prod need to heroku config set to finalize it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  after_create :welcome_send
+
   devise :database_authenticatable, :jwt_authenticatable, :registerable,
          :recoverable, jwt_revocation_strategy: JwtDenylist
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/
-  has_many :real_estate_ads, dependent: :destroy      
+  has_many :real_estate_ads, dependent: :destroy
+  
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
