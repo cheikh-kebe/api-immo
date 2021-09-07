22583dac9651fc5e21e05c9251225a1654fead47
shanks <cheikh.kebe.01@gmail.com>
2021-09-07 14:13:16 +0200

    reset password setted by the mailer doesn't work

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,12 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !') 
   end
+
+  def password_reset(user)
+    @user = user
+
+    @url  = 'http://localhost:3000/reset_password'
+
+    mail(to: @user.email, subject: "Password Reset")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   after_create :welcome_send
+  
 
   devise :database_authenticatable, :jwt_authenticatable, :registerable,
          :recoverable, jwt_revocation_strategy: JwtDenylist
@@ -12,5 +13,28 @@ class User < ApplicationRecord
   
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
+  end
+
+  def send_password_reset
+    self.password_reset_token = generate_base64_token
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_token_valid?
+    (self.password_reset_sent_at + 1.hour) > Time.zone.now
+  end
+
+  def reset_password(password)
+    self.password_reset_token = nil
+    self.password = password
+    save!
+  end
+
+  private
+
+  def generate_base64_token
+    test = SecureRandom.urlsafe_base64
   end
 end
