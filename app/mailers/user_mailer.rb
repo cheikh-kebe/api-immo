3ad8faf918f99fb9ac43bedc955750a47515eb34
shanks <cheikh.kebe.01@gmail.com>
2021-09-10 00:00:34 +0200

    smtp.rb init fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: '"https://api-immocoin.herokuapp.com"'
+  default from: 'https://api-immocoin.herokuapp.com'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
